0a0c1095c093b1d438ec495f088ede18bba3badc
PengTao <129124105+tpwxpk120@users.noreply.github.com>
2023-09-19 23:47:49 -0700

    Add files via upload

---
+++ js/appearfunction.js
@@ -0,0 +1,9 @@
+function revealContent() {
+    const content = document.getElementById('content');
+    content.style.opacity = '0';
+    setTimeout(function () {
+        content.style.transition = 'opacity 1s ease-in-out';
+        content.style.opacity = '1';
+    }, 5000); } 0
+
+    window.addEventListener('load', revealContent);
